28c740d87dd67767851912875c20de84b845a821
kristopherBr <kristo.bojilov@gmail.com>
2017-03-17 22:59:58 +0200

    Third change

--- Test/src/com/jigglypuff/application/TicTacCheckApplication.java
+++ Test/src/com/jigglypuff/application/TicTacCheckApplication.java
@@ -7,8 +7,18 @@ public class TicTacCheckApplication {
 	public static void main(String[] args) {
 		System.out.println("Another one... I mean, git push.");
 		String word = StaticObjects.input.nextLine();
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 4; i++)
+		{
 			System.out.println(i+word);
-		}
-	}
-}
+		}//for
+		
+		function();
+		
+	}//main
+	
+	public static void function()
+	{
+		System.out.println("Naaaaaa Sivaniaaaaa I have no ideaaaaa...");
+	}//function
+	
+}//class TicTacCheckApplication
