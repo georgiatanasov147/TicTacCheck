8a507b926d27cfa29adf49d7fafe9b99933307d5
kristopherBr <kristo.bojilov@gmail.com>
2017-03-17 22:30:39 +0200

    First ever change

--- Test/src/com/jigglypuff/application/TicTacCheckApplication.java
+++ Test/src/com/jigglypuff/application/TicTacCheckApplication.java
@@ -3,6 +3,6 @@ package com.jigglypuff.application;
 public class TicTacCheckApplication {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Another one... I mean, git push.");
 	}
 }
